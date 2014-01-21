af76baffe4f0ac39be90b1fc3a4139c7fda8f251
Linus Leong <comlinus@gmail.com>
2014-01-21 21:45:34 +0800

    added include directory & colors.h

---
+++ include/colors.h
@@ -0,0 +1,11 @@
+#ifndef H_COLORS
+#define H_COLORS
+
+#define RESET "\e[39;0m"
+#define RED "\e[31m"
+#define _RED "\e[31;1m"
+#define GREEN "\e[32m"
+#define _GREEN "\e[32;1m"
+#define YELLOW "\e[33m"
+
+#endif
