c15fd84a88404445f7a49a90248f77bb3427978a
Linus Leong <comlinus@gmail.com>
2014-01-21 16:03:31 +0800

    Added a simple help & Planned future options to work on

--- main.c
+++ main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include "colors.h"
 
+void printhelp(void);
+
 int main(int argc, char **argv)
 {
-	printf(GREEN "Hello World!" RESET);
+	char types[3][10] = {"celsius", "fahrenheit", "kelvin"};
+	int i;
+	if (argc < 3 && argv[2] != "-l")
+	{
+		printf(RED "Error: Wrong number of augument given!\n" RESET);
+		printhelp();
+		return 0;
+	}
 	return 0;
 }
+
+void printhelp(void)
+{
+	printf("Syntax: \ttemperature <options> <type(from)> <type(to)> <value>\n");
+	printf("Options:\t-a: print out a nicer list of all types(ignores type(to);\n");
+	printf("\t\t-l: list all types avaliable(ignores all arguments);\n");
+	printf("\t\t-c: show the set-by-step flow of the program;\n");
+	printf("\t\t-f: show all the formulas used in this program(ignores all arguments);\n");
+	printf("\t\t-h: show this help;\n");	
+}
+
